a63f4eb29d9e7018364334b4cc3a3f11e2237d57
Matthew <mmetros@wesleyan.edu>
2018-09-26 00:10:15 -0400

    Tuesday

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/meters')
+def index():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    query = "SELECT DISTINCT label, id FROM meters "
+    results = cursor.execute(query)
+    rows = cursor.fetchall()
+    print(rows)
+    print(rows[0][1])
+    return render_template('meters.html', meters=rows)
+
+@app.route('/meters/<string:id>')
+def meter_data(id):
+    # open up a connection
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    # create a query to access the meter_data
+    query = "SELECT * FROM meter_data WHERE meter_id=?"
+    result = cursor.execute(query,(id,))
+
+    row = result.fetchall()
+    connection.close()
+    return jsonify(row)
+
+
+# @app.route('/_background_process')
+# def _background_process():
+#     name = request.args.get('name')
+
+
+# @app.route('/meters/<string:name>')
+# def index():
+#     return render_template('')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ create_tables.py
@@ -0,0 +1,80 @@
+import sqlite3
+from datetime import datetime, date
+
+# For the current dateime
+now = datetime.now()
+
+connection = sqlite3.connect('data.db', detect_types=sqlite3.PARSE_DECLTYPES)
+cursor = connection.cursor()
+cursor.execute("PRAGMA foreign_keys = ON")
+
+# the id will always increment
+# INTEGER PRIMARY KEY will create auto incrementing columns
+# now when we have a new user, we will only have to create a username and password
+create_meters = "CREATE TABLE IF NOT EXISTS meters ( id INTEGER PRIMARY KEY, label text)"
+cursor.execute(create_meters)
+
+# The meters table is the parent
+# The meter_data table is the child
+
+create_meter_data = """CREATE TABLE IF NOT EXISTS meter_data (
+                        id INTEGER PRIMARY KEY,
+                        meter_id int,
+                        [timestamp] timestamp,
+                        value int,
+                        FOREIGN KEY (meter_id) REFERENCES meters(id) on delete cascade
+                        )"""
+
+cursor.execute(create_meter_data)
+
+query = "INSERT INTO meters VALUES (NULL,?)"
+
+meter_array = ["Meter1", "Meter2", "Meter3"]
+for meter in meter_array:
+    cursor.execute(query,(meter,))
+
+cursor.execute("INSERT INTO meter_data VALUES (NULL,?, ?, ?)", (1, now,10))
+cursor.execute("INSERT INTO meter_data VALUES (NULL,?, ?, ?)", (2, now, 20))
+cursor.execute("INSERT INTO meter_data VALUES (NULL,?, ?, ?)", (3, now, 40))
+
+
+
+
+connection.commit()
+connection.close()
+
+
+# Tests to see if the foreign key was successful
+def see_table1():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    query = "SELECT * FROM meters"
+    result = cursor.execute(query)
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+def see_table2():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    query = "SELECT * FROM meter_data"
+    result = cursor.execute(query)
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+
+def delete(id):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    query = "DELETE FROM meters WHERE id=?"
+    cursor.execute(query, (id,))
+    connection.commit()
+    connection.close()
+
+see_table1()
+see_table2()
+
+delete(1)
+
+see_table1()
+see_table2()
